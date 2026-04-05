6c2f5b4f973ff8909281a1c2473ff2a1235df45e
Anthony Z <anthonyzhao93@gmail.com>
2026-04-05 02:05:46 +0200

    fix(map): resolve Leaflet initialization and hydration issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: false, // Désactivé pour Leaflet en dev
 };
 
 module.exports = nextConfig;
